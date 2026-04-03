bf8868bb590989d31bcc7118b29983973e9d42f9
kprabhneet <kprabhneet0@gmail.com>
2026-04-03 14:08:37 +0530

    Fix ALLOWED_HOSTS error

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -30,8 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get('DEBUG','False') == 'True'
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
 
 # Application definition
@@ -87,9 +86,9 @@ WSGI_APPLICATION = 'blogapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'NAME': os.environ.get('blogdb_zhjm'),
+        'USER': os.environ.get('blogdb_zhjm_user'),
+        'PASSWORD': os.environ.get('4hoNTNck7UsNXARl8wtm8IIEAw31i9dw'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': '5432',
         'OPTIONS': {
